e3b117f19ac8cc975a37979e53d5006ea3115c30
minaev-aa <71834110+minaev-aa@users.noreply.github.com>
2021-02-06 16:50:15 +0300

    Update lesson1.cpp
    upd notes

--- lesson1.cpp
+++ lesson1.cpp
@@ -1,27 +1,24 @@
-// cpp2sem.cpp: îïðåäåëÿåò òî÷êó âõîäà äëÿ êîíñîëüíîãî ïðèëîæåíèÿ.
-//
-
 #include "stdafx.h"
-# include <iostream>
+#include <iostream>
 #include <cmath>
 
-void stars(int count){
+void stars(int count) {
 	for (int i = 0; i < count; i++) {
 		std::cout << "*";
 	}
 }
 
-int main(){
-	//çàäàíèå 1
+int main() {
+	//task 1
 	std::cout << "Hello, World!" << std::endl;
 	std::cout << "mipt" << 2016 << '.' << true << std::endl;
-	//çàäàíèå 2
+	//task 2
 	double katet1, katet2;
 	std::cin >> katet1;
 	std::cin >> katet2;
 	std::cout << sqrt((katet1 * katet1) + (katet2 * katet2)) << std::endl;
-	//çàäàíèå 3
-	int amount_stars;  //Êîëè÷åñòâî çâ¸çäî÷åê
+	//task 3
+	int amount_stars;  
 	std::cin >> amount_stars;
 	for (int n = 0; n < amount_stars; n++) {
 		stars(amount_stars);
@@ -29,21 +26,21 @@ int main(){
 	}
 
 	std::cout << std::endl;
-	//çàäàíèå 4
+	//task 4
 	for (int n = 0; n < amount_stars; n++) {
 		stars(n);
 		std::cout << std::endl;
 	}
 
 	std::cout << std::endl;
-	//çàäàíèå 5
+	//task 5
 	for (int n = 0; n < amount_stars; n++) {
 		stars(amount_stars-n);
 		std::cout << std::endl;
 	}
 
 	std::cout << std::endl;
-	//çàäàíèå 6
+	//task 6
 	for (int n = 0; n < amount_stars; n += 2) {
 		stars(amount_stars-n);
 		std::cout << std::endl;
