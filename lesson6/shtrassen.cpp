d42c0369d68c82f23cda5900b9fb51b90f738e95
minaev-aa <71834110+minaev-aa@users.noreply.github.com>
2021-04-12 00:06:32 +0300

    add wok shtrass

--- lesson6/shtrassen.cpp
+++ lesson6/shtrassen.cpp
@@ -1,11 +1,13 @@
 ﻿#include <iostream>
+#include <time.h> 
+#include <cstdlib>
 
-double** matrix(int n)
+long long** matrix(int n)
 {
-	double** a = new double* [n];
+	long long** a = new long long* [n];
 	for (int i = 0; i < n; i++)
 	{
-		a[i] = new double[n];
+		a[i] = new long long[n];
 		for (int j = 0; j < n; j++)
 		{
 			a[i][j] = 0;
@@ -14,21 +16,23 @@ double** matrix(int n)
 	return a;
 }
 
-double** input(int n) {
-	double** a = new double* [n];
-	std::cout << "Input:" << '\n';
-	for (int i = 0; i < n; i++)
+long long** input(int n, int f) {
+	long long** a = new long long* [f];
+	for (int i = 0; i < f; i++)
 	{
-		a[i] = new double[n];
-		for (int j = 0; j < n; j++)
+		a[i] = new long long[f];
+		for (int j = 0; j < f; j++)
 		{
-			std::cin >> a[i][j];
+			if(i<n)
+				a[i][j] = rand()%10;
+			else
+				a[i][j] = 0;
 		}
 	}
 	return a;
 }
 
-void output(int n, double** a) {
+void output(int n, long long** a) {
      std::cout<< '\n' <<"Output:"<< '\n';
      for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
@@ -38,8 +42,8 @@ void output(int n, double** a) {
     }     
 }
 
-double** multiply(double** A, double** B, int n) {
-	double** C = matrix(n); 
+long long** multiply(long long** A, long long** B, int n) {
+	long long** C = matrix(n); 
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {      
            for(int t=0; t<n; t++) {
@@ -50,38 +54,45 @@ double** multiply(double** A, double** B, int n) {
 	return C;
 }
 
-double** add(double** A, double** B, int n) {
-    double** C = matrix(n);
+long long** add(long long** A, long long** B, int n) {
+    long long** C = matrix(n);
     for(int i=0; i<n; i++)
         for(int j=0; j<n; j++)
             C[i][j] = A[i][j] + B[i][j];
     return C;
 }
 
-double** sub(double** A, double** B, int n) {
-    double** C = matrix(n);
+long long** sub(long long** A, long long** B, int n) {
+    long long** C = matrix(n);
     for(int i=0; i<n; i++)
         for(int j=0; j<n; j++)
             C[i][j] = A[i][j] - B[i][j];
     return C;
 }
 
-double** strassen(double** A, double** B, int n) {
+long long** strassen(long long** A, long long** B, int n) {
+	if (n == 1) {
+    long long** C = matrix(1);
+    C[0][0] = A[0][0] * B[0][0];
+    return C;
+	}	
+	
 	if (n < 64) {
-		double** C = matrix(n);
+		long long** C = matrix(n);
 		C = multiply(A,B,n);
 		return C;
 	}
-	double** C = matrix(n);
+	
+	long long** C = matrix(n);
 	int k = n/2;
-    double** A11 = matrix(k);
-	double** A12 = matrix(k);
-	double** A21 = matrix(k);
-	double** A22 = matrix(k);
-	double** B11 = matrix(k);
-	double** B12 = matrix(k);
-	double** B21 = matrix(k);
-	double** B22 = matrix(k);
+    long long** A11 = matrix(k);
+	long long** A12 = matrix(k);
+	long long** A21 = matrix(k);
+	long long** A22 = matrix(k);
+	long long** B11 = matrix(k);
+	long long** B12 = matrix(k);
+	long long** B21 = matrix(k);
+	long long** B22 = matrix(k);
 	for(int i=0; i<k; i++){
 		for(int j=0; j<k; j++) {
 			A11[i][j] = A[i][j];
@@ -94,18 +105,18 @@ double** strassen(double** A, double** B, int n) {
 			B22[i][j] = B[k+i][k+j];
 		}
 	} 
-    double** P1 = strassen(A11, sub(B12, B22, k), k);
-	double** P2 = strassen(add(A11, A12, k), B22, k);
-	double** P3 = strassen(add(A21, A22, k), B11, k);
-	double** P4 = strassen(A22, sub(B21, B11, k), k);
-	double** P5 = strassen(add(A11, A22, k), add(B11, B22, k), k);
-	double** P6 = strassen(sub(A12, A22, k), add(B21, B22, k), k);
-	double** P7 = strassen(sub(A11, A21, k), add(B11, B12, k), k);
+    long long** P1 = strassen(add(A11, A22, k), add(B11, B22, k), k);
+	long long** P2 = strassen(add(A21, A22, k), B11, k);
+	long long** P3 = strassen(A11, sub(B12, B22, k), k);
+	long long** P4 = strassen(A22, sub(B21, B11, k), k);
+	long long** P5 = strassen(add(A11, A12, k), B22, k);
+	long long** P6 = strassen(sub(A21, A11, k), add(B11, B12, k), k);
+	long long** P7 = strassen(sub(A12, A22, k), add(B21, B22, k), k);
 
-	double** C11 = sub(add(add(P5, P4, k), P6, k), P2, k);
-	double** C12 = add(P1, P2, k);
-	double** C21 = add(P3, P4, k);
-	double** C22 = sub(sub(add(P5, P1, k), P3, k), P7, k);
+	long long** C11 = add(sub(add(P1, P4, k), P5, k), P7, k);
+	long long** C12 = add(P3, P5, k);
+	long long** C21 = add(P2, P4, k);
+	long long** C22 = add(sub(add(P3, P1, k), P2, k), P6, k);
     for(int i=0; i<k; i++){
 		for(int j=0; j<k; j++) {
 			C[i][j] = C11[i][j];
@@ -114,18 +125,82 @@ double** strassen(double** A, double** B, int n) {
 			C[k+i][k+j] = C22[i][j];
 		}
 	}
+	for(int i=0; i<k; i++) {
+		delete[] A11[i];
+		delete[] A12[i];
+		delete[] A21[i];
+		delete[] A22[i];
+		delete[] B11[i];
+		delete[] B12[i];
+		delete[] B21[i];
+		delete[] B22[i];
+		delete[] P1[i];
+		delete[] P2[i];
+		delete[] P3[i];
+		delete[] P4[i];
+		delete[] P5[i];
+		delete[] P6[i];
+		delete[] P7[i];
+		delete[] C11[i];
+		delete[] C12[i];
+		delete[] C21[i];
+		delete[] C22[i];
+    }
+        
+    delete[] A11;
+    delete[] A12;
+    delete[] A21;
+    delete[] A22;
+    delete[] B11;
+    delete[] B12;
+    delete[] B21;
+    delete[] B22;
+    delete[] P1;
+    delete[] P2;
+    delete[] P3;
+    delete[] P4;
+    delete[] P5;
+    delete[] P6;
+    delete[] P7;
+    delete[] C11;
+    delete[] C12;
+    delete[] C21;
+    delete[] C22;
 	return C;
 }
 
 
 int main() {
 	std::cout << "Razmer kvadratnoy matrix: ";
-	int N;
+	int N,f;
 	std::cin >> N;
-	double** a = input(N);
-	double** b = input(N);
-	double** c = strassen(a, b, N);
-    output(N, c);
+
+	f = 1;
+	while(f<N)
+		f*=2;
+
+	long long** a = input(N, f);
+	long long** b = input(N, f);
+
+	clock_t start1 = clock();
+	long long** c1 = strassen(a, b, f);
+	clock_t end1 = clock();
+	double seconds1 = (double)(end1 - start1) / CLOCKS_PER_SEC;
+		
+	clock_t start2 = clock();
+	long long** c2 = multiply(a, b, N);
+	clock_t end2 = clock();
+	double seconds2 = (double)(end2 - start2) / CLOCKS_PER_SEC;
+
+	//output(N, a);
+	//output(N, b);
+
+	output(N, c1);
+	output(N, c2);
+	
+	std::cout << seconds1 << " " << seconds2 <<std::endl;
+	std::cout << std::endl;
+
     return 0;
 }
 
